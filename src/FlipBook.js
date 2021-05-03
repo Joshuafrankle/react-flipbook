deaf5a053e19aefadcb8c0d51011a63ced613f89
Joshuafrankle <joshuafrankle7@gmail.com>
2021-05-03 17:24:23 +0530

    Basic setup

---
+++ src/FlipBook.js
@@ -0,0 +1,147 @@
+import React, { Component } from "react";
+import HTMLFlipBook from "react-pageflip";
+
+import Image from "./assets/images/NewsLetter/00.png";
+import Image1 from "./assets/images/NewsLetter/01.png";
+import Image2 from "./assets/images/NewsLetter/02.png";
+import Image3 from "./assets/images/NewsLetter/03.png";
+import Image4 from "./assets/images/NewsLetter/04.png";
+import Image5 from "./assets/images/NewsLetter/05.png";
+import Image6 from "./assets/images/NewsLetter/06.png";
+import Image7 from "./assets/images/NewsLetter/07.png";
+import Image8 from "./assets/images/NewsLetter/08.png";
+import Image9 from "./assets/images/NewsLetter/09.png";
+import Image10 from "./assets/images/NewsLetter/10.png";
+import Image11 from "./assets/images/NewsLetter/11.png";
+import Image12 from "./assets/images/NewsLetter/12.png";
+import Image13 from "./assets/images/NewsLetter/13.png";
+import Image14 from "./assets/images/NewsLetter/14.png";
+import Image15 from "./assets/images/NewsLetter/15.png";
+import Image16 from "./assets/images/NewsLetter/16.png";
+import Image17 from "./assets/images/NewsLetter/17.png";
+import Image18 from "./assets/images/NewsLetter/18.png";
+import Image19 from "./assets/images/NewsLetter/19.png";
+import Image20 from "./assets/images/NewsLetter/20.png";
+import Image21 from "./assets/images/NewsLetter/21.png";
+import Image22 from "./assets/images/NewsLetter/22.png";
+import Image23 from "./assets/images/NewsLetter/23.png";
+import Image24 from "./assets/images/NewsLetter/24.png";
+import Image25 from "./assets/images/NewsLetter/25.png";
+import Image26 from "./assets/images/NewsLetter/26.png";
+import Image27 from "./assets/images/NewsLetter/27.png";
+import Image28 from "./assets/images/NewsLetter/28.png";
+import Image29 from "./assets/images/NewsLetter/29.png";
+import Image30 from "./assets/images/NewsLetter/30.png";
+import Image31 from "./assets/images/NewsLetter/31.png";
+
+export default class FlipBook extends Component {
+  render() {
+    return (
+      <section
+        className="justify-content-center align-items-center m-4 ml-5 pl-5"
+        styel={{ height: "100vh" }}
+      >
+        <div className="text-center">
+          <HTMLFlipBook width={450} height={600}>
+            <div className="demoPage">
+              <img src={Image} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image1} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image2} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image3} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image4} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image5} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image6} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image7} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image8} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image9} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image10} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image11} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image12} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image13} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image14} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image15} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image16} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image17} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image18} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image19} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image20} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image21} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image22} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image23} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image24} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image25} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image26} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image27} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image28} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image29} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image30} width="450" height="600" alt="" />
+            </div>
+            <div className="demoPage">
+              <img src={Image31} width="450" height="600" alt="" />
+            </div>
+          </HTMLFlipBook>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import FlipBook from "./FlipBook";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FlipBook />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
